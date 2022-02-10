60ffe9f2abf12b16a3ad8c5f7e38e698946edd9c
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-02-10 20:36:09 -0300

    Criando migration e model de usuario

---
+++ src/database/entities/entities.ts
@@ -0,0 +1,5 @@
+import { User } from "../../modules/accounts/model/User";
+import { Category } from "../../modules/cars/model/Category";
+import { Specification } from "../../modules/cars/model/Specification";
+
+export { User, Category, Specification };

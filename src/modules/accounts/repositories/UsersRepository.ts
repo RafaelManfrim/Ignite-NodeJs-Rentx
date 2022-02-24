ddfaf9ee027fe49ea7359b26ee3adaec0bcb0ad1
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-02-24 20:40:25 -0300

    Criptografando senha e impedindo email duplicado no user

--- src/modules/accounts/repositories/UsersRepository.ts
+++ src/modules/accounts/repositories/UsersRepository.ts
@@ -21,6 +21,11 @@ class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findByEmail(email: string) {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
 }
 
 export { UsersRepository };
--- src/modules/accounts/services/CreateUserService.ts
+++ src/modules/accounts/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import {
@@ -13,10 +14,18 @@ class CreateUserService {
   ) {}
 
   async execute({ name, email, password, driver_license }: ICreateUserDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }

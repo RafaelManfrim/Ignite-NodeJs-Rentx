f992e33a1305c675dff4d91840871c6b1a30da7c
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-02-27 08:29:04 -0300

    reescrevendo entities ormconfig.json, removendo arquivo entities

--- src/database/entities/entities.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "../../modules/accounts/model/User";
-import { Category } from "../../modules/cars/model/Category";
-import { Specification } from "../../modules/cars/model/Specification";
-
-export { User, Category, Specification };

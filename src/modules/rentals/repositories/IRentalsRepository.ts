4fbd0663c6464c84b999ad2f129a14671d5244f9
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-09 21:28:16 -0300

    IRentalsRepository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,13 @@
+import { Rental } from "../infra/typeorm/model/Rental";
+
+export interface ICreateRentalDTO {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+export interface IRentalsRepository {
+  create: (data: ICreateRentalDTO) => Promise<Rental>;
+  findByOpenRentalCarId: (car_id: string) => Promise<Rental>;
+  findOpenRentalByUser: (user_id: string) => Promise<Rental>;
+}

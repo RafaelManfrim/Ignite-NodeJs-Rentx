8d1da77491aeb1191da85f16e97f7c3ec3a5a23f
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-10 09:29:15 -0300

    trabalhando com conexão com banco de testes

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,5 +3,13 @@ import { createConnection, getConnectionOptions } from "typeorm";
 export default async (host = "localhost") => {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(Object.assign(defaultOptions, { host }));
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
+    })
+  );
 };

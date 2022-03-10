dcefb734c2ad49b24261813d5006de2293117914
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-09 21:29:58 -0300

    Registrando no container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,10 @@ import { ICarsImagesRepository } from "../../modules/cars/repositories/ICarsImag
 import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { RentalsRepository } from "../../modules/rentals/infra/repositories/RentalsRepository";
+import { IRentalsRepository } from "../../modules/rentals/repositories/IRentalsRepository";
+
+import "./providers";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -31,4 +35,9 @@ container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );

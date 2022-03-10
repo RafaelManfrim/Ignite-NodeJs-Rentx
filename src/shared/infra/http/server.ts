4e6a6082a77da4b5c7f4a01088ff9839097772a7
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-10 08:56:40 -0300

    exportando app e criando server separado

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,33 +1,3 @@
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-import "reflect-metadata";
-
-import swaggerFile from "../../../swagger.json";
-import { AppError } from "../../errors/AppError";
-import startConnection from "../typeorm";
-import { router } from "./routes";
-import "../../container";
-
-startConnection();
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({ message: error.message });
-  }
-
-  return res.status(500).json({
-    status: "error",
-    message: `Internal server error - ${error.message}`,
-  });
-});
+import { app } from "./app";
 
 app.listen(3333);

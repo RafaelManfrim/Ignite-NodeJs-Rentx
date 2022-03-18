da0ec28820385ffe5cde3e0be0781db26b02bb26
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-17 21:11:15 -0300

    UserTokens Model

---
+++ src/modules/accounts/infra/typeorm/model/UserTokens.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+
+import { User } from "./User";
+
+@Entity()
+export class UserTokens {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn()
+  user: User;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+}

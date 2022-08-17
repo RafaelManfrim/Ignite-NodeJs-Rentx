ef6633a24a3004b436e8ed80d216e600ffd20f6a
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-17 20:31:08 -0300

    ajuste upload de imagem de carro no S3

--- src/modules/cars/services/cars/UploadCarImageService.ts
+++ src/modules/cars/services/cars/UploadCarImageService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { ICarsImagesRepository } from "../../repositories/ICarsImagesRepository";
 
 interface IRequest {
@@ -11,12 +12,15 @@ interface IRequest {
 export class UploadCarImageService {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest) {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
